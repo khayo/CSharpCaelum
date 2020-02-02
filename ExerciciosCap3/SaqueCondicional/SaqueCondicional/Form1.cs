08e585563f33d0c7b2a4dfdd4dbc4373ff8fe376
Khayo Vannucci <khayovannucci@gmail.com>
2020-02-02 08:58:22 -0300

    Atividade saque condicional

---
+++ ExerciciosCap3/SaqueCondicional/SaqueCondicional/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaqueCondicional
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double saldo = 100.0;
+            double saque = 10.0;
+            if (saldo >= saque)
+            {
+                saldo -= saque;
+                MessageBox.Show("Saque realizado com sucesso!");
+            }
+            else
+            {
+                MessageBox.Show("Saldo Insuficiente");
+            }
+        }
+    }
+}
