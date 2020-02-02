ab7796d95dc7431d112618e5f3b9f4da6d0c5f6e
Khayo Vannucci <khayovannucci@gmail.com>
2020-02-02 12:29:20 -0300

    Multiplos de três

---
+++ ExerciciosCap5/MultiplosDeTres/MultiplosDeTres/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace MultiplosDeTres
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonExibir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonExibir
+            // 
+            this.buttonExibir.Location = new System.Drawing.Point(66, 25);
+            this.buttonExibir.Name = "buttonExibir";
+            this.buttonExibir.Size = new System.Drawing.Size(75, 23);
+            this.buttonExibir.TabIndex = 0;
+            this.buttonExibir.Text = "Exibir";
+            this.buttonExibir.UseVisualStyleBackColor = true;
+            this.buttonExibir.Click += new System.EventHandler(this.buttonExibir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Exibe os multiplos de 3 entre 1 e 100";
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(202, 56);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonExibir);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Multiplos de Três";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExibir;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ExerciciosCap5/MultiplosDeTres/MultiplosDeTres/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiplosDeTres
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExibir_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            List<int> listaMultiplos = new List<int>();
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    listaMultiplos.Add(i);
+                }
+
+
+            }
+            foreach (var item in listaMultiplos)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            MessageBox.Show("Multiplos de 3: \n" + sb);
+        }
+    }
+}
